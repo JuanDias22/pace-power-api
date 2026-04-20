3345879cd42bc7ffedfe127bceb0619635e9ce02
Juan Dias <fdias.juan@gmail.com>
2026-04-20 19:08:05 -0300

    feat: adiciona models de requisição de pagamento e webhook com validações

---
+++ PacePower.API/Controllers/PaymentsRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class PaymentsRequest
+{
+    [Required]
+    [RegularExpression("Basico|Intermediario|Premium")]
+    public string Plano { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    public string Nome { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
+
+public class MercadoPagoWebhook
+{
+    public string Type { get; set; }
+    public WebhookData Data { get; set; }
+}
+
+public class WebhookData
+{
+    public string Id { get; set; }
+}
