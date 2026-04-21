46b16547af30c2dd25e9b5be24345861e18326c0
Juan Dias <fdias.juan@gmail.com>
2026-04-21 01:45:52 -0300

    refactor: organizar o arquivo Program.cs e melhorar a configuração do CORS.

--- PacePower.API/Program.cs
+++ PacePower.API/Program.cs
@@ -5,37 +5,48 @@ using PacePower.API.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = new[]
+{
+    "https://pace-power-landing-page.vercel.app"
+};
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
+
 builder.Services.AddScoped<LeadService>();
 builder.Services.AddScoped<PaymentsRepository>();
 builder.Services.AddScoped<UserRepository>();
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
-
 var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseCors("AllowAll");
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
 
 app.MapControllers();
 
